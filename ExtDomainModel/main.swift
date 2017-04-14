8b101765f6568773e0601a4bef37e6cd1557155a
Vicky Juan <vicky37@uw.edu>
2017-04-14 00:21:05 -0700

    Initial Commit

---
+++ ExtDomainModel/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ExtDomainModel
+//
+//  Created by Vicky Juan on 4/14/17.
+//  Copyright © 2017 Vicky Juan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
